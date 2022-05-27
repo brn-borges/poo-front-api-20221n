45d8a8f0f2651f4d0622be4c20a96ac0b2afd418
Bruno Borges <bruninho-borgesf@hotmail.com>
2022-05-27 20:14:41 -0300

    Aula 27/05/2022 - front end com api v2

--- funcoes.js
+++ funcoes.js
@@ -1,4 +1,19 @@
 // vai requisitar uma aps
 async function cadastrar(){
+    // recuperar os dados do usuario
+    let nome = document.getElementById("nome").value
+    let tipo = document.getElementById("tipo").value
+    let poder= document.getElementById("poder").value
+    let nota = Number(document.getElementById("nota").value)
 
+    // criar o dado para enviar
+    let dado ={
+        nome, tipo, poder, nota
+    }  
+    // chamar ou consumir a API utilizando fetch
+    await fetch('http://localhost:8080/pokemon',{
+        method: 'POST',
+        body: JSON.stringify(dado),
+        headers: {"Content-Type": "application/json; charset=UTF-8"}
+    })
 }
